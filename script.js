fcaf4cad8c985e6292ae502080ec7f930b61e785
Chinmay <chinz.sandur07@gmail.com>
2023-12-07 16:22:55 +0530

    add all the declaration

---
+++ script.js
@@ -0,0 +1,7 @@
+const msg1 = document.querySelector("#msg1");
+const msg2 = document.querySelector("#msg2");
+const msg3 = document.querySelector("#msg3");
+
+const result = Math.floor(math.random() * 10) -1;
+const no_of_guesses = 0;
+let gussed_number = [];
